997659dcce27b39cccef86ea76d7750111da3827
Guzel Zaripova <guzel.zhagfarovna@gmail.com>
2024-10-30 20:40:39 +0300

    createCard + deleteCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+const placesContainer = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template");
 
-// @todo: DOM узлы
+function createCard({ name, link }, onDelete) {
+  const node = cardTemplate.content.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardTitle = node.querySelector(".card__title");
+  cardTitle.textContent = name;
 
-// @todo: Функция удаления карточки
+  const cardImage = node.querySelector(".card__image");
+  cardImage.setAttribute("src", link);
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = node.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", onDelete);
+
+  return node;
+}
+
+function handleDeleteCard(event) {
+  const placesItem = event.target.closest(".places__item");
+  placesItem.remove();
+}
+
+function appendCards() {
+  for (const item of initialCards) {
+    const card = createCard(item, handleDeleteCard);
+    placesContainer.append(card);
+  }
+}
+
+appendCards();
