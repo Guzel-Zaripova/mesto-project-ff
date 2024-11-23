a406cf15b54e820532a62b6ad62ae7fc768342e9
Guzel Zaripova <guzel.zhagfarovna@gmail.com>
2024-11-23 14:15:01 +0500

    Исправление метода работы с оверлеем

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -8,12 +8,6 @@ function openModal(element) {
     element.classList.add("popup_is-opened");
   }, waitingForAnimation);
 
-  function hanleOverlay(event) {
-    if (event.target === element) {
-      closeModal(element);
-      element.removeEventListener("click", hanleOverlay);
-    }
-  }
   element.addEventListener("click", hanleOverlay);
 
   function handleKeydown(event) {
@@ -27,6 +21,16 @@ function openModal(element) {
 
 function closeModal(element) {
   element.classList.remove("popup_is-opened");
+  element.removeEventListener("click", hanleOverlay);
+}
+
+function hanleOverlay(event) {
+  if (
+    event.target === event.currentTarget ||
+    event.target.classList.contains("popup__close")
+  ) {
+    closeModal(event.currentTarget);
+  }
 }
 
 export { openModal, closeModal };
